f3c09c52b4b29a90b1ececeb4d9664a8f1457428
sandy <sandy.ali.basha@gmail.com>
2023-11-07 11:58:18 +0200

    added mp4

--- src/pages/comingSoon/index.js
+++ src/pages/comingSoon/index.js
@@ -6,6 +6,7 @@ import Footer from "../../components/common/Footer";
 import videoBg from "../../assets/videos/coming-soon-bg.webm";
 import videoBgAvi from "../../assets/videos/coming-soon-bg.avi";
 import videoBgMov from "../../assets/videos/coming-soon-bg.mov";
+import videoBgMp4 from "../../assets/videos/coming-soon-bg.mp4";
 // styles
 import { SailingSoon } from "./sailingSoon";
 
@@ -22,6 +23,7 @@ export default function ComingSoon() {
         <source src={videoBg} type="video/webm" />
         <source src={videoBgAvi} type="video/avi" />
         <source src={videoBgMov} type="video/mov" />
+        <source src={videoBgMp4} type="video/mp4" />
 
       </video>
       <div className="absolute top-0 w-full h-full flex flex-col">
